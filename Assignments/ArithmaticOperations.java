8f82230a5df4766dc87c300c0d8b714c68155a2b
truppagar <145847555+truppagar@users.noreply.github.com>
2024-09-09 21:11:38 +0530

    Update ArithmaticOperations.java

--- Assignments/ArithmaticOperations.java
+++ Assignments/ArithmaticOperations.java
@@ -1,48 +1,48 @@
 import java.util.*;
 public class ArithmaticOperationsMenuDriven {
 
-	public static void main(String[] args) { 			//main method
+	public static void main(String[] args) { 			
 		
 		int num_1,num_2;
-		char choice;							// declaring variables
+		char choice;							
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("Enter first number:-\t"); 	//getting first number from user
+		System.out.println("Enter first number:-\t"); 
 		num_1=sc.nextInt();
 		
-		System.out.println("Enter second number:-\t"); 	//getting second number from user
+		System.out.println("Enter second number:-\t"); 
 		num_2=sc.nextInt();	
 		
-		do												// loop started (do-while)
+		do												
 		{
-			System.out.println("MENU");					//Menu to select 
+			System.out.println("MENU");					 
 			System.out.println("1.Addition (+)");
 			System.out.println("2.Subtraction (-)");
 			System.out.println("3.Multiplication (*)");
 			System.out.println("4.Division (/)");
 			System.out.println("5.Exit ($)");
 			System.out.println("Enter your Choice");
-			choice=sc.next().charAt(0);						//getting choice
+			choice=sc.next().charAt(0);						
 			
 			switch(choice)
 			{
-					case '+':System.out.println("Sum of "+num_1+" and "+num_2+" is "+(num_1+num_2)); //print sum
+					case '+':System.out.println("Sum of "+num_1+" and "+num_2+" is "+(num_1+num_2)); 
 							break;
 							
-					case '-':System.out.println("Subtraction of "+num_1+" and "+num_2+" is "+(num_1-num_2));	//print subtraction
+					case '-':System.out.println("Subtraction of "+num_1+" and "+num_2+" is "+(num_1-num_2));	
 							break;
 							
-					case '*':System.out.println("Multiplication of "+num_1+" and "+num_2+" is "+(num_1*num_2)); //print multiplication
+					case '*':System.out.println("Multiplication of "+num_1+" and "+num_2+" is "+(num_1*num_2));
 							break;
 						
-					case '/':System.out.println("Division of "+num_1+" and "+num_2+" is "+(num_1/num_2));		//print division
+					case '/':System.out.println("Division of "+num_1+" and "+num_2+" is "+(num_1/num_2));		
 							break;
 					
 					default:System.out.println("Invalid Choice or May Select Exit");
 							break;
 				
 			}
-		}while(choice!='$');							//terminating loop (do-while)
+		}while(choice!='$');							
 		
 	}
 
